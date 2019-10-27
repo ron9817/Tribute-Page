30b721a9c9c245e2fd33a40249e9f5d9ef934814
Your Name <you@example.com>
2019-10-27 13:46:35 +0000

    Solved issue Animations required for the transitions in the slideshow #3

--- js/main.js
+++ js/main.js
@@ -6,13 +6,19 @@ function changeImage(n) {
   if (currentImgIndex>5) { currentImgIndex = 1; }
   if (currentImgIndex<1) { currentImgIndex = 5; }
   showImage(currentImgIndex);
+  
 }
 
-function showImage(n) {
+function showImage(currentImgIndex){
+  $("#current_image").animate({opacity:'0'},2000,function(){
+    showImage2(currentImgIndex)
+  });
+}
+
+function showImage2(n) {
   console.log(n);
 
   var ourImage = document.getElementById("current_image");
-
   if(n==1) {
     $("#current_image").attr("src","images/slideshow1.jpg");
     $(".circle").css("background-color","white");
@@ -38,5 +44,5 @@ function showImage(n) {
     $(".circle").css("background-color","white");
     $("#image_index5").css("background-color","#ABA592");
     }
-
+  $("#current_image").css('opacity','1');
 }

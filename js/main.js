45038d5ad2c97aea6098919c4eed00322dcdb516
Your Name <you@example.com>
2019-10-29 19:50:45 +0000

    Tried second approach for slideshow. Hopefully its better

--- js/main.js
+++ js/main.js
@@ -1,42 +1,38 @@
 var currentImgIndex = 1;
-showImage(currentImgIndex);
-
 function changeImage(n) {
   currentImgIndex += n;
   if (currentImgIndex>5) { currentImgIndex = 1; }
   if (currentImgIndex<1) { currentImgIndex = 5; }
   showImage(currentImgIndex);
 }
-
 function showImage(n) {
-  console.log(n);
-
   var ourImage = document.getElementById("current_image");
-
   if(n==1) {
-    $("#current_image").attr("src","images/slideshow1.jpg");
+    $(".slideshow2").css("backgroundImage","url('images/slideshow1.jpg')");
     $(".circle").css("background-color","white");
     $("#image_index1").css("background-color","#ABA592");
   }
   if(n==2) {
-    $("#current_image").attr("src","images/slideshow2.jpg");
+    $(".slideshow2").css("backgroundImage","url('images/slideshow2.jpg')");
     $(".circle").css("background-color","white");
     $("#image_index2").css("background-color","#ABA592");
     }
   if(n==3) {
-    $("#current_image").attr("src","images/slideshow3.png");
+    $(".slideshow2").css("backgroundImage","url('images/slideshow3.png')");
     $(".circle").css("background-color","white");
     $("#image_index3").css("background-color","#ABA592");
     }
   if(n==4) {
-    $("#current_image").attr("src","images/slideshow4.jpg");
+    $(".slideshow2").css("backgroundImage","url('images/slideshow4.jpg')");
     $(".circle").css("background-color","white");
     $("#image_index4").css("background-color","#ABA592");
     }
   if(n==5) {
-    $("#current_image").attr("src","images/slideshow5.png");
+    $(".slideshow2").css("backgroundImage","url('images/slideshow5.png')");
     $(".circle").css("background-color","white");
     $("#image_index5").css("background-color","#ABA592");
     }
-
 }
+setInterval(function(){
+  changeImage(1)},5000
+  );
